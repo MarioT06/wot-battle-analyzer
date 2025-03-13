c2d7fdf64d15e383401e2020c1d83466d665ab07
MarioT06 <your.email@example.com>
2025-03-13 16:10:58 +0200

    Remove sudo commands from build script

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 # Install Chrome
 curl -LO "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-sudo dpkg -i google-chrome-stable_current_amd64.deb || true
-sudo apt-get update
-sudo apt-get install -f -y
+dpkg -i google-chrome-stable_current_amd64.deb || true
+apt-get update
+apt-get install -f -y
 rm google-chrome-stable_current_amd64.deb
 
 # Install ChromeDriver
@@ -12,6 +12,6 @@ CHROME_VERSION=$(google-chrome --version | cut -d ' ' -f3)
 CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION%%.*}")
 curl -LO "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/local/bin/
-sudo chmod +x /usr/local/bin/chromedriver
+mv chromedriver /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
 rm chromedriver_linux64.zip 
